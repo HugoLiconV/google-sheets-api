f482594c3df141a1d2a3eeee9d5976bc6a34e0f0
Hugo Licon <hugo_val28@hotmail.com>
2021-05-09 01:03:09 -0600

    add console log

--- src/config.js
+++ src/config.js
@@ -11,6 +11,7 @@ const requireProcessEnv = name => {
   if (!process.env[name]) {
     throw new Error('You must set the ' + name + ' environment variable')
   }
+  console.log(process.env[name])
   return process.env[name]
 }
 
