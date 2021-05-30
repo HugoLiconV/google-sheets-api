d5ec61ca9f8b796bfe4acf046a4fb46789a358d6
Hugo Licon <hugo_val28@hotmail.com>
2021-05-30 13:22:10 -0600

    add prettier

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const cors = require('cors');
-const compression = require('compression');
-const morgan = require('morgan');
-const { env, origin } = require('../../config')
+const express = require("express");
+const cors = require("cors");
+const compression = require("compression");
+const morgan = require("morgan");
+const { env, origin } = require("../../config");
 
 module.exports = (apiRoot, routes) => {
   const app = express();
   const corsOptions = {
-    origin: env === 'production' ? origin : 'http://localhost:3000'
+    origin: env === "production" ? origin : "*",
+  };
+  if (env === "production" || env === "development") {
+    app.use(cors(corsOptions));
+    app.use(compression());
+    app.use(morgan("dev"));
   }
-  if (env === 'production' || env === 'development') {
-    app.use(cors(corsOptions))
-    app.use(compression())
-    app.use(morgan('dev'))
-  }
 
-  app.use(express.urlencoded({ extended: false }))
-  app.use(express.json())
-  app.use(apiRoot, routes)
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use(apiRoot, routes);
 
-  return app
-
-}
+  return app;
+};
